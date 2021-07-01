446e85472b9f35e8416f1fa6858ae6c14f3dffc8
Sascha Lisson <sl@slisson.de>
2021-07-01 08:57:31 +0200

    model-api: More constructors for SimpleConcept

--- model-api/src/commonMain/kotlin/org/modelix/model/api/SimpleConcept.kt
+++ model-api/src/commonMain/kotlin/org/modelix/model/api/SimpleConcept.kt
@@ -26,6 +26,12 @@ class SimpleConcept(
     override val referenceLinks: MutableList<IReferenceLink> = ArrayList()
     private val superConcepts: List<IConcept> = directSuperConcepts.toList()
 
+    constructor(name: String): this(name, false, listOf())
+
+    constructor(name: String, isAbstract: Boolean): this(name, isAbstract, listOf())
+
+    constructor(name: String, directSuperConcepts: Iterable<IConcept>): this(name, false, directSuperConcepts)
+
     override fun isAbstract(): Boolean = this.isAbstract
 
     override fun getUID(): String = getLongName()
